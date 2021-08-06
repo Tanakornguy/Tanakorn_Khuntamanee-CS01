0ddca0522653b8ddc255cc7c5be391fbd0dde296
Tanakornguy <guytanakornkhuntamanee@gmail.com>
2021-08-06 09:56:56 +0700

    list

--- CS01-14.py
+++ CS01-14.py
@@ -1,7 +1,7 @@
 thislist = ['5','10','15','20','25','50','20']
 print(thislist)
 for i in range(len(thislist)):
-    print
+    print(i)
     if (thislist[i] == '20'):
         thislist[i] = '200'
 print(thislist)
